d1499d6d74c1c7c03c06b22dbb8d304c2267a5a4
Pablo Acebedo <pablofelipe@me.com>
2021-10-15 08:49:35 -0500

    10 day code chalenge day 2

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-console.log('100 days')
+// console.log('100 days')
 
-for (let i = 0; i <= 10; i++) {
+// for (let i = 0; i <= 10; i++) {
     
-    console.log(i)
+//     console.log(i)
+// }
+
+// const scores = [0, 10, 20, 30, 40, 50, 60]
+// for (let i = 0; i < scores.length; i++) {
+//     console.log (i, scores[i])
+    
+// }
+
+const scores2 = [
+    {
+        name: 'Pablo',
+        grade: 10
+    },
+
+    {
+        name: 'Zeus',
+        grade: 20
+    },
+
+    {
+        name: 'Pandora',
+        grade: 30
+    },
+
+    {
+        name: 'Lili',
+        grade: 40
+    }
+]
+
+for (let i = 0; i < scores2.length; i++) {
+   let student = scores2[i]
+    console.log(`${student.name} scored ${student.grade}`)
 }
